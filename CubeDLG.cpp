d2d0794dd4e5defa29ee569487823fc9e6b2ec28
Andrey <Uuetsukeu@mail.ru>
2024-04-05 23:44:00 +0300

    headers fixed? camera next?

--- CubeDLG.cpp
+++ CubeDLG.cpp
@@ -44,11 +44,11 @@ int CubeDlg::init() {
 
     shaders.resize(6);
         shaders[0] = load_shader("../main.glslv", "../main.glslf");
-        shaders[1] = load_shader("../back.glslv", "../back.glslf");
-        shaders[2] = load_shader("../left.glslv", "../left.glslf");
-        shaders[3] = load_shader("../right.glslv", "../right.glslf");
-        shaders[4] = load_shader("../up.glslv", "../up.glslf");
-        shaders[5] = load_shader("../bottom.glslv", "../bottom.glslf");
+        shaders[1] = load_shader("../main.glslv", "../back.glslf");
+        shaders[2] = load_shader("../main.glslv", "../left.glslf");
+        shaders[3] = load_shader("../main.glslv", "../right.glslf");
+        shaders[4] = load_shader("../main.glslv", "../up.glslf");
+        shaders[5] = load_shader("../main.glslv", "../bottom.glslf");
 
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GLFW_TRUE);
     return 0;
--- main.glslv
+++ main.glslv
@@ -2,7 +2,12 @@
 
 layout(location = 0) in vec3 vertexPosition_modelspace;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main(){
 	 gl_Position.xyz = vertexPosition_modelspace;
      gl_Position.w = 1.0;
+     //gl_Position = projection * view * model * gl_Position;
 }
